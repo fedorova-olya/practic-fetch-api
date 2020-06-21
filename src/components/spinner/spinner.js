b5ae2cca6655f4400704848d26549f4f19ed6f2a
Olya <fedorovaolya0410@gmail.com>
2020-06-21 14:15:13 +0300

    last change 21.06.2020

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-spinner-kl73qrv7g2q">
+      <div class="ldio-21bqv4r0lka">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
